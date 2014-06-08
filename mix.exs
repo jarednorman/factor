e477553763d5cab6425696df4b561662b06c430f
Jared Norman <jared@freerunningtechnologies.com>
2014-06-08 10:42:53 -0700

    Specify register in mix.exs
    Change-Id: I62f97d6d9b355afa3275cb4ba083d364f9c88877

--- mix.exs
+++ mix.exs
@@ -2,16 +2,16 @@ defmodule Factor.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :factor,
-     version: "0.0.1",
-     elixir: "~> 0.13.3",
-     deps: deps]
+    [ app: :factor,
+      version: "0.0.1",
+      elixir: "~> 0.13.3",
+      deps: deps ]
   end
 
   # Configuration for the OTP application
   def application do
-    [applications: [],
-     mod: {Factor, []}]
+    [ registered: [:factor],
+      mod: {Factor, []} ]
   end
 
   defp deps do
