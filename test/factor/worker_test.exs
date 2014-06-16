149c28f2e247488c36331a8e579eb36a41ea79b7
Jared Norman <jared@freerunningtechnologies.com>
2014-06-16 07:56:04 -0700

    Improve test coverage of worker
    Change-Id: Icc98e02c168797f91e6a89b1b09d9e11f65972c5

--- test/factor/worker_test.exs
+++ test/factor/worker_test.exs
@@ -1,6 +1,12 @@
 defmodule FactorWorkerTest do
   use ExUnit.Case
 
+  test "it initializes with no games" do
+    { response, initial } = Factor.Worker.init
+    assert response == :ok
+    assert Enum.count(initial) == 0
+  end
+
   test "it handles new_game events" do
     state = Factor.GameCollection.new
 
@@ -23,5 +29,20 @@ defmodule FactorWorkerTest do
 
     assert Factor.Worker.handle_call({ :end_turn, 0 }, nil, state) ==
       { :reply, :johnny, expected_state }
+  end
+
+  test "it handles add_move events" do
+    game = Factor.Game.new
+      |> Factor.Game.add_move(:denis, 1)
+
+    { _, state  } = Factor.GameCollection.new
+      |> Factor.GameCollection.add_game(game)
+
+    game = Factor.Game.add_move(game, :jared, 100)
+    { _, expected_state } = Factor.GameCollection.new
+      |> Factor.GameCollection.add_game(game)
+
+    assert Factor.Worker.handle_cast({ :add_move, 0, :jared, 100 }, state) ==
+      { :noreply, expected_state }
   end
 end
