5a90fb8ab9814b707432cfa15a9a14140aed05a8
Jared Norman <jared@freerunningtechnologies.com>
2014-06-16 08:00:56 -0700

    Improve functional test
    Change-Id: I45f3fc1626fe40921b9ffdab713bcb53c7168f35

--- test/factor_test.exs
+++ test/factor_test.exs
@@ -2,14 +2,29 @@ defmodule FactorTest do
   use ExUnit.Case
 
   test "it makes games happen" do
-    game = Factor.new_game
-    Factor.add_move(game, :player_one, 2)
-    Factor.add_move(game, :player_two, 3)
-    winner = Factor.end_turn(game)
-    assert winner == :player_two
-    Factor.add_move(game, :player_one, 4)
-    Factor.add_move(game, :player_two, 1)
-    winner = Factor.end_turn(game)
-    assert winner == :player_one
+    game_one = Factor.new_game
+
+    Factor.add_move(game_one, :john, 3)
+    winner = Factor.end_turn(game_one)
+    assert winner == :john
+
+    game_two = Factor.new_game
+
+    Factor.add_move(game_one, :jared, 4)
+    Factor.add_move(game_one, :alistair, 1)
+    winner = Factor.end_turn(game_one)
+    assert winner == :jared
+
+    Factor.add_move(game_two, :amy, 4)
+    Factor.add_move(game_two, :cave, 5)
+    Factor.add_move(game_two, :gordon, 100)
+    Factor.add_move(game_two, :denis, 12)
+    winner = Factor.end_turn(game_two)
+    assert winner == :gordon
+
+    Factor.add_move(game_one, :amy, 4)
+    Factor.add_move(game_one, :denis, 12)
+    winner = Factor.end_turn(game_one)
+    assert winner == :denis
   end
 end
